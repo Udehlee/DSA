e28c3c12513e5e365440d1f7c7e7061d6374bbc6
udehlee <ritzeepita@gmail.com>
2024-07-31 20:03:19 +0100

    add binary search to search algorithm

---
+++ Algorithm/search/binarySearch.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+)
+
+// iterative way
+// from lowest to highest, in a sorted manner
+// if the middle number is the value given return true
+// if the value givn is less than the middle number, move towards the startNum
+// if the value given is more than the middle number move towards the endNum
+func BinarySearch(data []int, val int) bool {
+	startNum := 0
+	endNum := len(data) - 1
+
+	for startNum <= endNum {
+		midNum := startNum + (endNum-startNum)/2
+
+		if data[midNum] == val {
+			return true
+		} else if midNum < val {
+			startNum++
+		} else {
+			endNum--
+
+		}
+	}
+	return false
+}
+
+// if the data in row & col equals value, return true
+// if its greater than value move to previous col
+// else move to next row
+func FindElement2DArray(data [][]int, row, col, val int) bool {
+	initialRow := 0
+	column := col - 1
+
+	for initialRow < row && column >= 0 {
+		if data[row][col] == val {
+			return true
+		} else if data[row][col] > val {
+			col--
+		} else {
+			row++
+		}
+
+	}
+	return false
+}
+func main() {
+
+	s := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	fmt.Println(BinarySearch(s, 2))
+
+}
