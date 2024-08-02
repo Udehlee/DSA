db3580810fd76cf679df01c43a5c2c20af3ecfb1
udehlee <ritzeepita@gmail.com>
2024-08-02 19:51:16 +0100

    add stack push and pop functionality

---
+++ Data-structures/stack/linkedlistStack.go
@@ -0,0 +1,24 @@
+package main
+
+type Stack struct {
+	head  *Node
+	count int
+}
+
+type Node struct {
+	value int
+	next  *Node
+}
+
+func (s *Stack) Push(value int) {
+	s.head = &Node{value, s.head}
+	s.count++
+
+}
+
+func (s *Stack) Pop() int {
+	toRemove := s.head.value
+	s.head = s.head.next
+	return toRemove
+
+}
---
+++ Data-structures/stack/sliceStack.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+type stack struct {
+	arr []int
+}
+
+// push adds value to the stack
+func (s *stack) Push(value int) {
+	s.arr = append(s.arr, value)
+
+}
+
+// pop removes value from the stack
+func (s *stack) Pop() int {
+	toRemove := s.arr[len(s.arr)-1]
+	s.arr = s.arr[:len(s.arr)-1]
+	return toRemove
+
+}
+
+func (s *stack) Print() {
+	for i := 0; i < len(s.arr); i++ {
+		fmt.Println(s.arr[i])
+	}
+}
+
+func main() {
+
+	s := &stack{}
+	s.Push(2)
+	s.Push(3)
+	s.Push(4)
+
+	s.Print()
+
+	fmt.Println("------------")
+
+	s.Pop()
+	s.Print()
+
+}
